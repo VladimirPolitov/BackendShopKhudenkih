fa6bff450f1dedf1e4b491566b0488b17bc6ca00
ADMIN <v-ol-kov@yandex.ru>
2022-03-17 14:53:03 +0500

    add deviceItem

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import {Col, Container} from "react-bootstrap";
+import Row from "react-bootstrap/Row"
+import TypeBar from "../components/TypeBar";
+import BrandBar from "../components/BrandBar";
+import DeviceList from "../components/DeviceList";
 
 const Shop = () => {
     return (
-        <div>
-            SHOP
-        </div>
+        <Container className='mt-2'>
+            <Row>
+                <Col md={3}>
+                    <TypeBar/>
+                </Col>
+                <Col md={9}>
+                    <BrandBar/>
+                    <DeviceList/>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -4,11 +4,15 @@ export default class DeviceStore {
     constructor() {
         this._types = [
             {id: 1, name: 'Холодильник'},
-            {id: 2, name: 'Смартфоны'}
+            {id: 2, name: 'Смартфоны'},
+            {id: 3, name: 'Пылесосы'},
+            {id: 4, name: 'Колготки'},
         ]
         this._brands = [
             {id: 1, name: 'Samsung'},
-            {id: 2, name: 'Apple'}
+            {id: 2, name: 'Apple'},
+            {id: 3, name: 'Lenovo'},
+            {id: 4, name: 'Asus'},
         ]
         this._devices = [
             {id: 1, name: 'Iphone 12pro', price: 25000, rating: 5, img: 'https://cdn.svyaznoy.ru/upload/iblock/08a/iphone_13_pro_max_q421_graphite_pdp_image_position-2__ru-ru.jpg'},
@@ -16,6 +20,8 @@ export default class DeviceStore {
             {id: 3, name: 'Iphone 12pro', price: 25000, rating: 5, img: 'https://cdn.svyaznoy.ru/upload/iblock/08a/iphone_13_pro_max_q421_graphite_pdp_image_position-2__ru-ru.jpg'},
             {id: 4, name: 'Iphone 12pro', price: 25000, rating: 5, img: 'https://cdn.svyaznoy.ru/upload/iblock/08a/iphone_13_pro_max_q421_graphite_pdp_image_position-2__ru-ru.jpg'},
         ]
+        this._selectedType = {}
+        this._selectedBrand = {}
         makeAutoObservable(this)
     }
 
@@ -28,6 +34,12 @@ export default class DeviceStore {
     setDevices(devices) {
         this._devices = devices
     }
+    setSelectedType(type) {
+        this._selectedType = type
+    }
+    setSelectedBrand(brand) {
+        this._selectedBrand = brand
+    }
 
     get types() {
         return this._types
@@ -37,5 +49,11 @@ export default class DeviceStore {
     }
     get devices() {
         return this._devices
+    }
+    get selectedType() {
+        return this._selectedType
+    }
+    get selectedBrand() {
+        return this._selectedBrand
     }
 }
