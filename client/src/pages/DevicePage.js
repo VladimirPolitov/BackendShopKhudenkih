48985ed7f00cffb47b40e7cf7491931810a15cf8
ADMIN <v-ol-kov@yandex.ru>
2022-03-19 13:45:46 +0500

    add device only

--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import {Button, Card, Col, Container, Image, Row} from "react-bootstrap";
 
 const DevicePage = () => {
+    const device = {
+        id: 1,
+        name: 'Iphone 12pro',
+        price: 25000,
+        rating: 5,
+        img: 'https://cdn.svyaznoy.ru/upload/iblock/08a/iphone_13_pro_max_q421_graphite_pdp_image_position-2__ru-ru.jpg'
+    }
+    const description = [
+        {id: 1, title: 'Оперативная память', description: '5Гб'},
+        {id: 2, title: 'Камера', description: '5Гб'},
+        {id: 3, title: 'Процессор', description: '5Гб'},
+        {id: 4, title: 'Ядра', description: '5Гб'},
+        {id: 5, title: 'Аккумулятор', description: '5Гб'}
+    ]
     return (
-        <div>
-            DevicePage
-        </div>
+        <Container className='mt-3'>
+            <Row>
+                <Col md={4}>
+                    <Image width={300} height={300} src={device.img}/>
+                </Col>
+
+                <Col md={4}>
+                    <div className='d-flex flex-column align-items-center'>
+                        <h2>{device.name}</h2>
+                        <div className='d-flex align-items-center justify-content-center'>
+                            {device.rating}
+                        </div>
+                    </div>
+                </Col>
+
+                <Col md={4}>
+                    <Card className='d-flex flex-column align-items-center justify-content-around'
+                    style={{width: 300, height:300, fontSize: 32, border:' 5px solid lightgray'}}
+                    >
+                        <h3>От: {device.price} руб.</h3>
+                        <Button variant={'outline-dark'}>Добавить в корзину</Button>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
